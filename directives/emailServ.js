dfd71b27a6f721f5fed32ec7e9a65a15abe1965f
Nick Marucci <npm1514@gmail.com>
2016-01-09 18:40:40 -0700

    Email works

--- directives/emailServ.js
+++ directives/emailServ.js
@@ -1,5 +1,5 @@
 angular.module("solSiteRouter").service("emailServ", function($http) {
-  var toUrl = 'npm1514@gmail.com';
+
   this.sendEmail = function(name, email, message) {
     if (name && email && message) {
       return $http({
@@ -8,17 +8,15 @@ angular.module("solSiteRouter").service("emailServ", function($http) {
           data: {
             'key': 'kLu16ufqZK6L8u1zYNyj6Q',
             'message': {
-              'from_email': email,
-              'to': {
-                'email': toUrl,
+              'from_email': 'npmarucci@sol-innovations.com',
+              'to': [{
+                'email': 'npm1514@gmail.com',
                 'name': 'Sol Innovations',
                 'type': 'to'
-              },
+              }],
               'autotext': 'true',
-              'subject': 'Customer Inquiry - ' + name,
-              'html': 'to Nick' +
-                'from ' + name +
-                message
+              'subject': 'Customer Inquiry - ' + name + "@" + email,
+              'text': message
             }
           }
         })
